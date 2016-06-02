a672e497fbe5f5a8e438a8ccb7376761eb5c120b
Jorge Vega Ramirez <jorgevp5@gmail.com>
2016-06-02 13:37:25 -0400

    Initial Commit

---
+++ app.rb
@@ -0,0 +1,71 @@
+require 'sinatra/base'
+require 'sinatra/json'
+require 'json'
+
+class EventApp < Sinatra::Base
+  set :logging, true
+  set :show_exceptions, false
+  error do |e|
+    binding.pry
+    raise e
+  end
+
+  DB = {}
+
+  before do
+    require_authorization!
+  end
+
+  get "/list" do
+    DB[username] ||= []
+    json DB[username]
+  end
+
+  post "/list" do
+    body = request.body.read
+
+    begin
+      new_item = JSON.parse body
+    rescue
+      status 400
+      halt "Can't parse json: '#{body}'"
+    end
+
+    if new_item["title"]
+      DB[username] ||= []
+      DB[username].push new_item
+      body "ok"
+    else
+      status 422
+      body "No title"
+    end
+  end
+
+  patch "/list" do
+    title = params[:title]
+    DB[username] ||= []
+    existing_item = DB[username].find { |i| i["title"] == title }
+    if existing_item
+      DB[username].delete existing_item
+      status 200
+    else
+      status 404
+    end
+  end
+
+  def require_authorization!
+    unless username
+      status 401
+      halt({ error: "You must log in" }.to_json)
+    end
+  end
+
+  def username
+    request.env["HTTP_AUTHORIZATION"]
+  end
+end
+
+# if $0 == __FILE__
+if $PROGRAM_NAME == __FILE__
+  EventApp.run!
+end
---
+++ tests.rb
@@ -0,0 +1,105 @@
+require 'pry'
+require 'minitest/autorun'
+require 'minitest/focus'
+
+require 'minitest/reporters'
+Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
+
+require 'rack/test'
+
+require "./app"
+
+class EventAppBase < Minitest::Test
+  include Rack::Test::Methods
+
+  def app
+    EventApp
+  end
+
+  def setup
+    EventApp::DB.clear
+  end
+end
+
+# class NotLoggedIn < EventAppBase
+#   def test_login_is_required
+#     response = get "/list"
+#     assert_equal 401, response.status
+#
+#     body = JSON.parse response.body
+#     assert_equal "You must log in", body["error"]
+#   end
+#
+#   def test_user_lists_are_separate
+#     header "Authorization", "jdabbs"
+#     post "/list", '{"title": "groceries"}'
+#
+#     header "Authorization", "daniel"
+#     post "/list", '{"title": "learn ruby"}'
+#
+#     header "Authorization", "jdabbs"
+#     response = get "/list"
+#
+#     body = JSON.parse response.body
+#
+#     assert_equal 1, body.count
+#     assert_equal "groceries", body.first["title"]
+#   end
+# end
+#
+# class LoggedIn < EventAppBase
+#   def setup
+#     super
+#     header "Authorization", "user"
+#   end
+#
+#   def test_starts_with_empty_list
+#     response = get "/list"
+#
+#     assert_equal 200, response.status
+#     assert_equal "[]", response.body
+#   end
+#
+#   def test_can_add_to_list
+#     post "/list", '{"title": "groceries"}'
+#     post "/list", '{"title": "learn ruby"}'
+#
+#     response = get "/list"
+#
+#     assert_equal 200, response.status
+#
+#     list = JSON.parse response.body
+#     assert_equal 2, list.count
+#     assert_equal "groceries", list.first["title"]
+#   end
+#
+#   def test_add_response
+#     response = post "/list", '{"title": "do things"}'
+#     assert_equal 200, response.status
+#     assert_equal "ok", response.body
+#   end
+#
+#   def test_requires_title
+#     response = post "/list", '{}'
+#     assert_equal 422, response.status
+#     assert_equal "No title", response.body
+#   end
+#
+#   def test_handles_bad_json
+#     response = post "/list", 'not json'
+#     assert_equal 400, response.status
+#     assert_equal "Can't parse json: 'not json'", response.body
+#   end
+#
+#   def test_can_mark_items_complete
+#     post "/list", '{"title": "groceries"}'
+#     post "/list", '{"title": "learn ruby"}'
+#
+#     response = patch "/list", title: "learn ruby"
+#     assert_equal 200, response.status
+#
+#     response = get "/list"
+#     json = JSON.parse response.body
+#     assert_equal 1, json.count
+#   end
+# end
