018bbe60d322f6f16b26416f6e45d8f4c662c598
Jorge Vega Ramirez <jorgevp5@gmail.com>
2016-06-03 14:42:50 -0400

    before merging

--- forecast.rb
+++ forecast.rb
@@ -2,34 +2,44 @@ require 'pry'
 require "json"
 require "./weatherParser"
 
+
+  a =  File.read("weather.txt").chomp
+  data = JSON.parse a
+
 class Forecast
-  Data = []
+  # Data = JSON.parse(File.read("weather.txt").chomp)
 
-attr_reader :day, :month
-  def initialize(zipcode)
-    @day = ["forecast"]["simpleforecast"]["forecastday"][0]["date"]["day"]
-    @month = ["forecast"]["simpleforecast"]["forecastday"][0]["date"]["month"]
+  attr_reader :day, :month, :year, :high, :low
+
+  def initialize month:, day:, year:, high:, low:
+    @month = month
+    @day = day
+    @year = year
+    @high = high
+    @low = low
   end
 
 
-  raw = weather_data(zipcode)
-  def high
-    raw["forecast"]["simpleforecast"]["forecastday"].first["high"]["fahrenheit"]
-  end
+  #
+  # def high
+  #   @data["forecast"]["simpleforecast"]["forecastday"].first["high"]["fahrenheit"]
+  # end
+  #
+  # def low
+  #   @data["forecast"]["simpleforecast"]["forecastday"].first["low"]["fahrenheit"]
+  # end
 
-  def low
-    raw["forecast"]["simpleforecast"]["forecastday"].first["low"]["fahrenheit"]
-  end
-
-# use this to when combining information to match day and month to get weather
-# for the particular day
-#   def event_day_check
-#     e = Event.new
-#     f = Forecast.new(zipcode)
-#   if e.day && a.month == f.day && f.month
-#     return weather_data
-#   else
-#     return "Can't predict the future."
-#   end
-# end
+  # use this to when combining information to match day and month to get weather
+  # for the particular day
+  #   def event_day_check
+  #     e = Event.new
+  #     f = Forecast.new(zipcode)
+  #   if e.day && a.month == f.day && f.month
+  #     return weather_data
+  #   else
+  #     return "Can't predict the future."
+  #   end
+  # end
 end
+# 
+# t = Forecast.new data
